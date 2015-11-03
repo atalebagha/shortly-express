9b94444eb7ca5789e5c77f903de056775b7ffc3e
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2015-11-02 18:57:34 -0800

    added User Model and added reference to user id in link model

--- app/models/user.js
+++ app/models/user.js
@@ -3,6 +3,25 @@ var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
 var User = db.Model.extend({
+  tableName: 'users',
+  initialize:  function () {
+    this.on('creating', this.hashPassword, this);
+  },
+  links:  function () {
+    return this.hasMany(Link);
+  },
+  hashPassword: function (model, attr, options) {
+    return new Promise (function (resolve, reject){
+      bcrypt.hash(model.attributes.password, 8, function (err, hash) {
+        if (err) reject(err);
+        model.set('password', hash);
+        console.log(hash);
+        resolve(hash);
+      });
+
+    })
+  }
+
 });
 
 module.exports = User;
