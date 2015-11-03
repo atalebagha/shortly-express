7f96677177b9f6555a276d4e0f42a37b244a7569
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2015-11-02 20:00:00 -0800

    update hashing and initializing functions

--- app/models/user.js
+++ app/models/user.js
@@ -1,26 +1,39 @@
 var db = require('../config');
-var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
+var bcrypt = Promise.promisifyAll(require('bcrypt'));
 
 var User = db.Model.extend({
   tableName: 'users',
   initialize:  function () {
-    this.on('creating', this.hashPassword, this);
+    this.on('saving', this.hashPassword, this);
   },
+
+  validateSave: function() {
+    return checkit(rules).run(this.attributes);
+  },
+
   links:  function () {
     return this.hasMany(Link);
   },
-  hashPassword: function (model, attr, options) {
-    return new Promise (function (resolve, reject){
-      bcrypt.hash(model.attributes.password, 8, function (err, hash) {
-        if (err) reject(err);
+  hashPassword: function(model, attrs, options) {
+    return new Promise( function (resolve, reject) {
+      bcrypt.hash(model.attributes.password, 7, function (err, hash) {
+        if( err ) reject(err);
         model.set('password', hash);
-        console.log(hash);
-        resolve(hash);
+        resolve(hash); // data is created only after this occurs
       });
+    });
+  },
 
-    })
-  }
+  comparePassword: function (model, attr, options) {
+    return new Promise (function (resolve, reject){
+      bcrypt.compare(model.attributes.password, this.password, function (err, result) {
+        if (err) reject(err);
+        console.log(result);
+        resolve(result);
+      });
+    });
+  },
 
 });
 
